f61cca0f2215e07cbf21f8b1be6c9ba29fb2d65f
Ulvu11 <13593@holbertonstudents.com>
2026-04-11 13:53:46 -0400

    Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -44,9 +44,10 @@ class Square:
     def my_print(self):
         """Prints the square with the # character and position"""
         if self.__size == 0:
-            print("")
+            print()
             return
 
-        [print("") for i in range(self.__position[1])]
+        for i in range(self.__position[1]):
+            print()
         for i in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
