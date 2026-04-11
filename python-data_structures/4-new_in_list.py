71094d07ae1c87a1a24038d27874506841bf818f
Ulvu11 <13593@holbertonstudents.com>
2026-04-11 08:32:17 -0400

    Task 4: Write a function that replaces an element in a list at a specific position without modifying the original list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    a=my_list
+    for i in my_list:
+        if idx >= len(my_list) or idx < 0:
+            return a
+        a[idx] = element
+        return a
