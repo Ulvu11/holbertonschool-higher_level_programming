09501469fb5ac94b500c3b366c9ee1aa0e00429b
Ulvu11 <13593@holbertonstudents.com>
2026-04-15 13:43:55 +0000

    Write a new task about Exceptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
-        for i in my_list:
+        for i in range(x):
             print("{:d}".format(i))
             count += 1
     except Exception:
