adb10a8ad639dc7c9d3f0973ea7b77df7883dd7e
Ulvu11 <13593@holbertonstudents.com>
2026-04-15 13:51:29 +0000

    Write a new task about Exceptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[i]), end = "")
         except Exception:
             continue
-
+    print()
     return count
