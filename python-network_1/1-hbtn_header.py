7bd2ebd9a3bb659ed284a9ae3421097221a0a463
ulvu11 <13593@holbertonstudents.com>
2026-04-23 16:33:23 +0300

    Write a new task about Python - Network#1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,16 +4,16 @@ import sys
 import urllib.request
 
 def fetch_request_id(url):
-    """This module sends a request to a URL and displays a specific header value."""
-        request = urllib.request.Request(
+    """This module sends a request to a URL and displays a specific header value"""
+    request = urllib.request.Request(
         url,
         headers={"'cfclearance'": "true"}
     )
-        with urllib.request.urlopen(request) as response:
-                    request_id = response.headers.get("X-Request-Id"")
-                    print(request_id)
+    with urllib.request.urlopen(request) as response:
+        request_id = response.headers.get("X-Request-Id"")
+        print(request_id)
 def main():
-    """Get the URL from the command line and call the request function."""
+    """Get the URL from the command line and call the request function"""
     url = sys.argv[1]
     fetch_request_id(url)
 
