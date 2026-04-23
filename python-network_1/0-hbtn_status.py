1fbaa5b7bcfb85502196c07939843813b65ac191
ulvu11 <13593@holbertonstudents.com>
2026-04-23 14:06:17 +0300

    Write a new task about Python - Network#1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """This script fetches a specific URL status using urllib"""
+
 import urllib.request
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
-
-
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
