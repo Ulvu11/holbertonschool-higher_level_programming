43faf002dd41ce1d17cc2700471aaed4c0678924
ulvu11 <13593@holbertonstudents.com>
2026-04-24 17:58:11 +0300

    Write a new task about Python - Network#1

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -6,9 +6,6 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    
-    # Using the .get() method as required by your project's General instructions
     request_id = response.headers.get('X-Request-Id')
-    
     if request_id is not None:
         print(request_id)
